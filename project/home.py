72136d89e8a6dbac52eba1720f8974b5f668f0d5
Kevin Geng <khg@andrew.cmu.edu>
2020-06-15 05:16:18 -0400

    Copy home and course views

--- project/home.py
+++ project/home.py
@@ -3,19 +3,27 @@ from flask import (
 )
 from flask_login import login_required, current_user
 
+from .models import Course, CourseUser
+
 
 bp = Blueprint('home', __name__)
 
 
-@bp.route('/', methods=('GET',))
+@bp.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('home/index.html')
 
 
-@bp.route('/bla', methods=('GET',))
-def bla():
-    print(current_user)
-    return 'hello: {}'.format(current_user.is_authenticated)
+@bp.route('/course/<course_code>/')
+@login_required
+def course_detail(course_code):
+    course = Course.query.filter_by(code=course_code).first()
+    return render_template(
+        'home/course_detail.html',
+        course=course,
+        my_course_user=None,
+    )
 
 
 @bp.route('/protected', methods=('GET',))
